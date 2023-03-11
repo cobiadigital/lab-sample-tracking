016c78a67a71fbb10f995d51872e7a25f49ed9bd
Ben Brenner <ben.brenner@gmail.com>
2023-03-11 16:31:20 -0600

    search query labels and to download

--- cultures/make_labels.py
+++ cultures/make_labels.py
@@ -4,6 +4,7 @@ import os
 import io
 from flask import current_app
 from cultures.upload import upload_file
+from flask import send_file
 
 
 def makelabel(items):
@@ -11,14 +12,16 @@ def makelabel(items):
                                default_stylesheets=(os.path.join(current_app.instance_path,'B33-59-0.5x1.css'),))
     records = []
     for item in items:
-        print(item['title'])
         records.append(dict(sample_title=item['title'], sample_date=item['date'], sample_comment=item['comment'],
                             sample_lab=item['name'], sample_initials=item['initials'], sample_full_id=item['full_id']))
     filename = datetime.now().strftime("%Y%m%d-%H%M") + 'labels.pdf'
     bytes_pdf = label_writer.write_labels(records)
-    print(type(bytes_pdf))
     label_pdf = io.BytesIO(bytes_pdf)
-    print(type(label_pdf))
-    final_url = upload_file(label_pdf, filename)
-
-    return final_url
+    return send_file(
+        bytes_pdf,
+        as_attachment=True,
+        download_name=filename,
+        mimetype='application/pdf'
+    )
+    # final_url = upload_file(label_pdf, filename)
+    # return final_url
